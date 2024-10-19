3047f3d6e2530372b53e33c91dca42d0c812b254
Mufaddal <mufaddaltopiwala15@gmail.com>
2024-10-19 16:04:08 +0530

    homescreen done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import Header from '../component/Homepage/Header'
-import Cards from '../component/Homepage/Cards'
+import React from "react";
+import Header from "../component/Homepage/Header";
+import Cards from "../component/Homepage/Cards";
+import Header3 from "../component/Homepage/Header3";
+import Footer from "../component/Homepage/Footer";
 const App = () => {
   return (
     <>
-    <Header/>
-    <Cards/>
+      <Header />
+      <Cards />
+      <Header3 />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;

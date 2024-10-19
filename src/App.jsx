85906da268f5c95235b690e2aece894c6ee27d21
Mufaddal <mufaddaltopiwala15@gmail.com>
2024-10-19 11:05:35 +0530

    initial prototype

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../component/Homepage/Header'
+import Cards from '../component/Homepage/Cards'
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Cards/>
+    </>
+  )
+}
+
+export default App
